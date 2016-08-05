77f8add03fefb0c5edb070ab506526fd5c90c1e5
gabrielmirandat <gabrielmirandatt@gmail.com>
2016-08-05 13:27:58 -0300

    Added general Worker thread to run processes.

---
+++ common/src/Worker.cc
@@ -0,0 +1,23 @@
+#include "Worker.h"
+
+Worker::Worker(const QString &program, const QStringList &argList)
+: program_(program), argList_(argList)
+{
+    launcher_ = new Launcher(this);
+}
+
+Worker::~Worker()
+{
+    delete launcher_;
+}
+
+//public slot
+void Worker::doWork()
+{
+    QString result;
+    this->launcher_->launch(this->program_, this->argList_);
+
+    result = "Deu";
+    qDebug() << "DEEEUUU";
+    emit resultReady(result);
+}
