2914bbe16a00adbea4c80a41d8bb1595d770b4ad
gabrielmirandat <gabrielmirandatt@gmail.com>
2016-08-05 13:11:19 -0300

    Added #communication header file.

---
+++ #communication/include/Communication.h
@@ -0,0 +1,43 @@
+#ifndef COMMUNICATION_H_
+#define COMMUNICATION_H_
+
+#include <iostream>
+#include <QThread>
+
+//for shared access
+#include <QSharedMemory>
+#include <QBuffer>
+#include <QDataStream>
+#include "../common/data/visionData.h"
+
+class Communication
+{
+private:
+	int comm_counter;
+
+    //for shared access
+    QSharedMemory vision_shm;
+    visionData vision_data;
+    QBuffer buffer;
+    QDataStream in;
+
+    //singletone constructor
+    explicit Communication();
+    static Communication* instance;
+
+    void listen();
+    void receive();
+    void send();
+    void run();
+
+    //for shared access
+    void updateData();
+    void updateBuffer();
+    void getFromSharedMem();
+
+public:
+	virtual ~Communication();
+    static Communication* getInstance();
+};
+
+#endif //COMMUNICATION_H_
